c8aa38832f9840c73446fdbdf5444b83ffdca8a9
Rakib Ahmed <131173915+devRokib@users.noreply.github.com>
2024-09-23 21:10:06 +0600

    Add files via upload

---
+++ math.js
@@ -0,0 +1,16 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+const nmbr = 6.458
+
+console.log(Math.abs(nmbr))
+console.log(Math.floor(nmbr))
+console.log(Math.ceil(nmbr))
+console.log(Math.round(nmbr))
+console.log(Math.pow(3,2))
+console.log(Math.pow(2,6))
+console.log(Math.max(100, 300, 50, 400))
+console.log(Math.min(40, 50, 10,400,300))
+console.log(Math.sqrt(9))
+console.log(Math.sqrt(64))
+console.log(Math.round(Math.random() *50 + 1))
